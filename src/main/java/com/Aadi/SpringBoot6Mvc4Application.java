e2e23f3fbcdadf33c77a0fbd920e5a50604ef442
Aaditya_coder❤️ <mohiteaadi10@gmail.com>
2026-03-05 23:42:14 +0530

    just started project

---
+++ src/main/java/com/Aadi/SpringBoot6Mvc4Application.java
@@ -0,0 +1,13 @@
+package com.Aadi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6Mvc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot6Mvc4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Aadi/SpringBoot6Mvc4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aadi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot6Mvc4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
